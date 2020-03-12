8323e151bcc8c62739d85d3293e29dcc222986b7
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-03-13 04:36:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,4 +8,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.88.88/g' package/base-files/files/bin/config_generate
 #sed -i 's/4.14/4.19/g' target/linux/ramips/Makefile
-sed -i 'a src-git passwall https://github.com/Lienol/openwrt-package/tree/passwall' feeds.conf.default
+#sed -i 'a src-git passwall https://github.com/Lienol/openwrt-package/tree/passwall' feeds.conf.default
