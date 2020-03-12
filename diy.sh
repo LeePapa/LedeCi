f900a37f88b5a65c98a7ade4143c8d2d26cb2a29
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-03-13 01:52:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,5 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.88.88/g' package/base-files/files/bin/config_generate
-#sed -i 's/4.14/4.19/g' target/linux/ramips/Makefile
+sed -i 's/4.14/4.19/g' target/linux/ramips/Makefile
 sed '$asrc-git Lienol https://github.com/Lienol/openwrt-package/tree/passwall' feeds.conf.default
