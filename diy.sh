d2a30e50158cfd7afe27b0cfaacf0a9b59010a29
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-03-16 02:50:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,4 +9,14 @@
 sed -i 's/192.168.1.1/192.168.88.88/g' package/base-files/files/bin/config_generate
 #sed -i 's/4.14/4.19/g' target/linux/ramips/Makefile
 #sed -i 'a src-git passwall https://github.com/Lienol/openwrt-package/tree/passwall' feeds.conf.default
-git clone -b passwall https://github.com/Lienol/openwrt-package.git package/Lienol
+#git clone -b passwall https://github.com/Lienol/openwrt-package.git package/Lienol
+git clone https://github.com/Lienol/openwrt-package.git package/Lienol
+rm -rf feeds/lienol/lienol/ipt2socks
+rm -rf feeds/lienol/lienol/shadowsocksr-libev
+rm -rf feeds/lienol/lienol/pdnsd-alt
+rm -rf feeds/lienol/package/verysync
+rm -rf feeds/lienol/lienol/luci-app-verysync
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
