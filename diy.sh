180fa1f76c355acd4cf3f4df429832221fe8eda4
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-05-07 01:06:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,21 +9,28 @@
 sed -i 's/192.168.1.1/192.168.88.88/g' package/base-files/files/bin/config_generate
 #sed -i 's/4.14/4.19/g' target/linux/ramips/Makefile
 
-# PassWall
+########## PassWall ##########
 #sed -i 'a src-git passwall https://github.com/Lienol/openwrt-package/tree/passwall' feeds.conf.default
 #git clone -b passwall https://github.com/Lienol/openwrt-package.git package/Lienol
-git clone https://github.com/Lienol/openwrt-package.git package/Lienol
-rm -rf feeds/lienol/lienol/ipt2socks
-rm -rf feeds/lienol/lienol/shadowsocksr-libev
-rm -rf feeds/lienol/lienol/pdnsd-alt
-rm -rf feeds/lienol/package/verysync
-rm -rf feeds/lienol/lienol/luci-app-verysync
-rm -rf package/lean/luci-app-kodexplorer
-rm -rf package/lean/luci-app-pppoe-relay
-rm -rf package/lean/luci-app-pptp-server
-rm -rf package/lean/luci-app-v2ray-server
 
-# SmartDns 开始
+git clone https://github.com/Lienol/openwrt-package.git package/Lienol
+#rm -rf feeds/lienol/lienol/ipt2socks
+#rm -rf feeds/lienol/lienol/shadowsocksr-libev
+#rm -rf feeds/lienol/lienol/pdnsd-alt
+#rm -rf feeds/lienol/package/verysync
+#rm -rf feeds/lienol/lienol/luci-app-verysync
+#rm -rf package/lean/luci-app-kodexplorer
+#rm -rf package/lean/luci-app-pppoe-relay
+#rm -rf package/lean/luci-app-pptp-server
+#rm -rf package/lean/luci-app-v2ray-server
+
+git clone https://github.com/LeePapa/openwrt-package.git leetemp/Lienol
+mv leetemp/Lienol/lienol/luci-app-passwall/* package/Lienol/lienol
+
+
+########## PassWall结束 ##########
+
+########## SmartDns 开始 ##########
 #cd ..
 #git clone https://github.com/pymumu/smartdns.git pymumu
 #cd smartdns-orig
@@ -55,7 +62,7 @@ rm -rf package/lean/luci-app-v2ray-server
 #./scripts/feeds update -a
 #./scripts/feeds install -a
 
-#/SmartDns 结束
+########## /SmartDns 结束 ##########
 
 #SmartDns 官方方法
 
