2039fa37c0ccce244e3f3f27bcde894a21ed39a5
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-05-17 12:15:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -105,24 +105,24 @@ sed -i "s/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=master/g" $WORKINGDIR/Makef
 
 #利用第三方法,安装最新版luci
 
-cd ..
-git clone https://github.com/pymumu/smartdns.git pymumu
-cd smartdns-orig
+#cd ..
+#git clone https://github.com/pymumu/smartdns.git pymumu
+#cd smartdns-orig
 
-FROM="../pymumu"
+#FROM="../pymumu"
 
 #18版本使用
-TO="luci-compat"
-cp -rf $FROM/package/luci-compat/files/etc/ $TO/root/
-cp -rf $FROM/package/luci-compat/files/luci/view/ $TO/luasrc/
-cp -rf $FROM/package/luci-compat/files/luci/controller/ $TO/luasrc/
-cp -rf $FROM/package/luci-compat/files/luci/model/ $TO/luasrc/
-cp -rf $FROM/package/luci-compat/files/luci/i18n/* $TO/po/zh-cn/
+#TO="luci-compat"
+#cp -rf $FROM/package/luci-compat/files/etc/ $TO/root/
+#cp -rf $FROM/package/luci-compat/files/luci/view/ $TO/luasrc/
+#cp -rf $FROM/package/luci-compat/files/luci/controller/ $TO/luasrc/
+#cp -rf $FROM/package/luci-compat/files/luci/model/ $TO/luasrc/
+#cp -rf $FROM/package/luci-compat/files/luci/i18n/* $TO/po/zh-cn/
 
-sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(date +'%Y%m%d')/g" $TO/Makefile #修改版本号为日期
+#sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(date +'%Y%m%d')/g" $TO/Makefile #修改版本号为日期
 
-cp -rf luci-compat ../openwrt/feeds/luci/applications/luci-app-smartdns
-cd ../openwrt
+#cp -rf luci-compat ../openwrt/feeds/luci/applications/luci-app-smartdns
+#cd ../openwrt
 
 #利用第三方法,安装最新版luci 结束
 
