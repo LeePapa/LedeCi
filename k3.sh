02882fcc2cc338dae19a5ec395cdb896e82a408a
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-05-17 09:18:31 +0800

    Update k3.sh

--- k3.sh
+++ k3.sh
@@ -1,1 +1,2 @@
-
+sed -i 's/4.14/4.19/g' target/linux/bcm53xx/Makefile #更改编译内核为4.19
+sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3|' target/linux/bcm53xx/image/Makefile #仅编译K3
