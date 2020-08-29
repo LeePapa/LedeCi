49a10d6240a7d49f9172fb08f43014a1cf99a227
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-08-30 00:22:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,6 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #去掉makefile版本限制，使用最新源码
+WORKINGDIR="feeds/packages/net/smartdns"
 sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(date +'%Y%m%d%H%M')/g" $WORKINGDIR/Makefile #修改版本号为日期
 sed -i "s/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=master/g" $WORKINGDIR/Makefile #删除版本标识
