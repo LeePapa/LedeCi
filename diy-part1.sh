453f33e81d0e8df9be427024dc474ded25353f9e
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-09-02 03:59:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,11 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #去掉makefile版本限制，使用最新源码
-#WORKINGDIR="feeds/packages/net/smartdns"
-#sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(date +'%Y%m%d%H%M')/g" $WORKINGDIR/Makefile #修改版本号为日期
-#sed -i "s/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=master/g" $WORKINGDIR/Makefile #删除版本标识
+WORKINGDIR="feeds/packages/net/smartdns"
+sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(date +'%Y%m%d%H%M')/g" $WORKINGDIR/Makefile
+sed -i "s/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=master/g" $WORKINGDIR/Makefile
+#利用第三方法,安装最新版smartdnsluci
+cd ..
+cd smartdns-orig
+cp -rf luci-compat ../openwrt/feeds/luci/applications/luci-app-smartdns/
+cd ../openwrt
