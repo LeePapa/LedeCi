a94f065533b2abaa6c611d3bba0e76652da6310f
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-09-02 21:16:22 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -108,25 +108,25 @@
 
 #利用第三方法,安装最新版luci
 
-  cd ..
-  git clone https://github.com/pymumu/smartdns.git pymumu
-  cd smartdns-orig
+#  cd ..
+#  git clone https://github.com/pymumu/smartdns.git pymumu
+#  cd smartdns-orig
 
-  FROM="../pymumu"
-  TEM="../openwrt/feeds/luci/applications/luci-app-smartdns"
+#  FROM="../pymumu"
+#  TEM="../openwrt/feeds/luci/applications/luci-app-smartdns"
 #18版本使用
-  TO="luci-compat"
-  cp -rf $FROM/package/luci-compat/files/etc/ $TO/root/
-  cp -rf $FROM/package/luci-compat/files/luci/view/ $TO/luasrc/
-  cp -rf $FROM/package/luci-compat/files/luci/controller/ $TO/luasrc/
-  cp -rf $FROM/package/luci-compat/files/luci/model/ $TO/luasrc/
-  cp -rf $FROM/package/luci-compat/files/luci/i18n/* $TO/po/zh-cn/
-  mkdir $TEM
-  cp -rf luci-compat ../openwrt/feeds/luci/applications/luci-app-smartdns/
+#  TO="luci-compat"
+#  cp -rf $FROM/package/luci-compat/files/etc/ $TO/root/
+#  cp -rf $FROM/package/luci-compat/files/luci/view/ $TO/luasrc/
+#  cp -rf $FROM/package/luci-compat/files/luci/controller/ $TO/luasrc/
+#  cp -rf $FROM/package/luci-compat/files/luci/model/ $TO/luasrc/
+#  cp -rf $FROM/package/luci-compat/files/luci/i18n/* $TO/po/zh-cn/
+#  mkdir $TEM
+#  cp -rf luci-compat ../openwrt/feeds/luci/applications/luci-app-smartdns/
   
   #mkdir -p ../openwrt/feeds/luci/applications/luci-app-smartdns
-  sed -i "s#PKG_VERSION:=.*#PKG_VERSION:=$(date +'%Y%m%d')#" ../openwrt/feeds/luci/applications/luci-app-smartdns/Makefile
-  cd ../openwrt
+#  sed -i "s#PKG_VERSION:=.*#PKG_VERSION:=$(date +'%Y%m%d')#" ../openwrt/feeds/luci/applications/luci-app-smartdns/Makefile
+#  cd ../openwrt
 
 #利用第三方法,安装最新版luci 结束
 
