cdb0db86631a173247af88568f529d5819242ee8
LeePapa <61931198+LeePapa@users.noreply.github.com>
2020-09-02 14:46:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -119,9 +119,9 @@
   cp -rf $FROM/package/luci-compat/files/luci/controller/ $TO/luasrc/
   cp -rf $FROM/package/luci-compat/files/luci/model/ $TO/luasrc/
   cp -rf $FROM/package/luci-compat/files/luci/i18n/* $TO/po/zh-cn/
+  cp -rf ./luci-compat/ ../openwrt/feeds/luci/applications/luci-app-smartdns/
   
   #mkdir -p ../openwrt/feeds/luci/applications/luci-app-smartdns
-  /cp -rf luci-compat ../openwrt/feeds/luci/applications/luci-app-smartdns/
   sed -i "s#PKG_VERSION:=.*#PKG_VERSION:=$(date +'%Y%m%d')#" ../openwrt/feeds/luci/applications/luci-app-smartdns/Makefile
   cd ../openwrt
 
